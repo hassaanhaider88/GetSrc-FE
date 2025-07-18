deb7caf4873cceba90b9d5399223ef9d35b31800
hassaan haider <hassaanhaider088@gmail.com>
2025-07-18 06:25:48 +0500

    Improve logics

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -51,7 +51,7 @@ const HomePage = () => {
           content="Learn about GetURI - the user can access to all uploaded media and Copy and download other's Media"
         />
       </Helmet>
-      <section className="w-full min-h-screen px-10 py-10">
+      <section className="w-full min-h-screen px-5 py-10 sm:px-10">
         {DummyData == [] ? (
           <div role="status" aria-live="polite" className="text-center text-xl">
             Loading...
@@ -84,7 +84,7 @@ const HomePage = () => {
                 <button
                   type="button"
                   onClick={() => setOpenSeachTab(!openSeachTab)}
-                  className="flex items-center gap-2 bg-[#00A7CB] text-white px-4 py-2 rounded-full  shadow hover:bg-[#00A7CB]"
+                  className="flex items-center gap-2 bg-[#4ED7DD] text-white px-4 py-2 rounded-full  shadow hover:bg-[#4ed6ddd7]"
                 >
                   {selectedSeachTab} {/* Show selected option */}
                   <FaChevronDown
