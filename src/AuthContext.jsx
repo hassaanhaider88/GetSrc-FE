e197e02f2c8ff4aedf118dd50d3092ab28d80ea9
hassaanhaider88 <hassaanhaider088@gmail.com>
2026-03-31 13:13:22 +0500

    adding netlify.toml file

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext, useEffect, useState } from "react";
 import GetSrcBE from "../API.js";
 import FirstData from "./DummyData/File.json";
